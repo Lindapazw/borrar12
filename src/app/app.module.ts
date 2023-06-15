e19a1b4f525b82dd74810298d4258fa0106d8849
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-15 12:49:49 -0300

    Fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Angular Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavegacionComponent } from './navegacion/navegacion.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
-// Para ruteo
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegacionComponent,
     DashboardComponent,
+    NavegacionComponent
+
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
@@ -39,7 +37,8 @@ import { SharedModule } from './shared/shared.module';
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
-    SharedModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
